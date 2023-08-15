3f4686cd3d376101650a1f810783d8e96ce63efd
Shayan Shiari <shayanshiari@gmail.com>
2023-08-16 00:11:19 +0430

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+};

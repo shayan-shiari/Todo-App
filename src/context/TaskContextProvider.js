e269b02af463ebdd08c0717e612b6dcf9764f212
Shayan Shiari <shayanshiari@gmail.com>
2023-08-16 22:26:57 +0430

    localStorage added

--- src/context/TaskContextProvider.js
+++ src/context/TaskContextProvider.js
@@ -1,7 +1,7 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
 import { addTask } from "../helper/reducerFunctions";
 
-const initialState = {
+const initialState = JSON.parse(localStorage.getItem("tasks")) || {
   todoTasks: [
     "Start with meditation, exercise & breakfast for a productive day",
     "Read to learn something new every day",
@@ -29,6 +29,11 @@ export const taksContext = createContext(); //context
 // this component wrapped all components to use context value
 const TaskContextProvider = ({ children }) => {
   const [tasks, dispatch] = useReducer(reducer, initialState);
+
+  // when state(tasks) changes useEffect save state in localStorage
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   return (
     <taksContext.Provider value={{ tasks, dispatch }}>
